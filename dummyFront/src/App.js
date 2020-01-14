4369724581a0ee65d4e09fc0ba99c255b396be83
Alejandro Quesada <alejandro.quesada.next.contractor@bbva.com>
2020-01-14 16:57:55 +0100

    Fix: front local storage

--- dummyFront/src/App.js
+++ dummyFront/src/App.js
@@ -7,7 +7,12 @@ import Edit from './components/edit.component';
 import Index from './components/index.component';
 import Login from './components/login.component';
 
+localStorage.setItem("name", "Admin");
+localStorage.setItem("actions", []);
+localStorage.setItem("isLogged", true);
+
 class App extends Component {
+
   render() {
     return (
       <Router>
